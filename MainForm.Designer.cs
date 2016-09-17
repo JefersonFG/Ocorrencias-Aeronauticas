c96f93da98a541fe57bc09627f566ecce415ebb5
Jéferson Ferreira Guimarães <jefersonfguimaraes@gmail.com>
2016-09-17 10:35:58 -0300

    Ajustado código de centralização de mapa e inserção de marcador
    Foi adicionado código de exemplo de setup inicial com centralização do mapa por coordenada ou palavras-chave e inserção de marcador por coordenada.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,20 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Gmap = new GMap.NET.WindowsForms.GMapControl();
             this.SuspendLayout();
+            // 
+            // Gmap
+            // 
+            this.Gmap.Bearing = 0F;
+            this.Gmap.CanDragMap = true;
+            this.Gmap.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Gmap.EmptyTileColor = System.Drawing.Color.RoyalBlue;
+            this.Gmap.GrayScaleMode = false;
+            this.Gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.Gmap.LevelsKeepInMemmory = 5;
+            this.Gmap.Location = new System.Drawing.Point(0, 0);
+            this.Gmap.MarkersEnabled = true;
+            this.Gmap.MaxZoom = 18;
+            this.Gmap.MinZoom = 2;
+            this.Gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.Gmap.Name = "Gmap";
+            this.Gmap.NegativeMode = false;
+            this.Gmap.PolygonsEnabled = true;
+            this.Gmap.RetryLoadTile = 0;
+            this.Gmap.RoutesEnabled = true;
+            this.Gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.Gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.Gmap.ShowTileGridLines = false;
+            this.Gmap.Size = new System.Drawing.Size(484, 361);
+            this.Gmap.TabIndex = 0;
+            this.Gmap.Zoom = 13D;
+            this.Gmap.Load += new System.EventHandler(this.Gmap_Load);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.Gmap);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl Gmap;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Namespaces do GMap.NET
+
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+
 namespace Ocorrências_Aeronáuticas
 {
     public partial class MainForm : Form
@@ -15,6 +22,26 @@ namespace Ocorrências_Aeronáuticas
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void Gmap_Load(object sender, EventArgs e)
+        {
+            //Setup inicial feito no evento OnLoad do componente de mapas
+
+            Gmap.MapProvider = GoogleMapProvider.Instance;              //Utilizando Google Maps como provider
+            GMaps.Instance.Mode = AccessMode.ServerOnly;                //Configurado para não criar cache, usar sempre informações do servidor
+            //Gmap.SetPositionByKeywords("Porto Alegre, Brazil");       //Posição por palavras chave
+            Gmap.Position = new PointLatLng(-30.0364242, -51.2191413);  //Posição por coordenadas
+            Gmap.ShowCenter = false;                                    //Remove a cruz que indica o centro do mapa
+
+            //Inserção de um marcado de local
+
+            GMapOverlay markers = new GMapOverlay("markers");           //Overlay é uma camada onde colocamos marcadores e rotas
+            GMapMarker marker = new GMarkerGoogle(
+                new PointLatLng(-30.0364242, -51.2191413),
+                GMarkerGoogleType.red_small);                           //Configurado marcador por coordenada
+            markers.Markers.Add(marker);                                //Adicionado o marcador ao overlay
+            Gmap.Overlays.Add(markers);                                 //Adicionado overlay ao mapa
         }
     }
 }
