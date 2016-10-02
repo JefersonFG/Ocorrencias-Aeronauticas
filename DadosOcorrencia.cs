c04306d0f3b4d476ac8357df5535a79c7ea85de5
Jéferson Ferreira Guimarães <jefersonfguimaraes@gmail.com>
2016-10-02 18:28:18 -0300

    Removida classe redundante Registro e adicionada parte da documentação

--- DadosOcorrencia.cs
+++ DadosOcorrencia.cs
@@ -6,11 +6,51 @@ using System.Threading.Tasks;
 
 namespace Ocorrências_Aeronáuticas
 {
-    class DadosOcorrencia
+    /// <summary>
+    /// Classe que agrega os dados da ocorrência lida dos três arquivos, cada uma no seu objeto
+    /// A classe será usada no dicionário que por sua vez será usado para ordenação dos dados
+    /// Os dados podem ser ordenados pelo campo codigo_ocorrencia ou localidade
+    /// </summary>
+    public class DadosOcorrencia
     {
+        /// <summary>
+        /// Código de identificação de ocorrência, comum aos três ojetos
+        /// </summary>
+        /// <value>
+        /// Valor inteiro
+        /// </value>
         public int codigo_ocorrencia { get; set; }
+
+        /// <summary>
+        /// Cidade / município onde houve a ocorrência, presente no objeto ocorrencia
+        /// </summary>
+        /// <value>
+        /// String contendo o nome do município por extenso
+        /// </value>
+        public string localidade { get; set; }
+
+        /// <summary>
+        /// Objeto contendo os dados lidos do arquivo aeronave.csv com o codigo_ocorrencia acima
+        /// </summary>
+        /// <value>
+        /// Objeto do tipo Aeronave
+        /// </value>
         public Aeronave aeronave { get; set; }
+
+        /// <summary>
+        /// Objeto contendo os dados lidos do arquivo ocorrencia.csv com o codigo_ocorrencia acima
+        /// </summary>
+        /// <value>
+        /// Objeto do tipo Ocorrencia
+        /// </value>
         public Ocorrencia ocorrencia { get; set; }
+
+        /// <summary>
+        /// Objeto contendo os dados lidos do arquivo fator_contribuinte.csv com o codigo_ocorrencia acima
+        /// </summary>
+        /// <value>
+        /// Objeto do tipo FatorContribuinte
+        /// </value>
         public FatorContribuinte fator { get; set; }
     }
 }
